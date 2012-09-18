138b864bdd7ae3f7302708f3a784b18fbdbac551
Mattia Belletti <mattia.belletti@gmail.com>
2012-09-18 21:51:33 +0200

    Documentation.

--- MagicRipper/SetCardsDownloadingEventArgs.cs
+++ MagicRipper/SetCardsDownloadingEventArgs.cs
@@ -5,12 +5,13 @@ using System.Text;
 
 namespace MagicRipper
 {
+    /// <summary>
+    /// Provides data for the <c>Ripper.ExpansionCardsDownloading</c> event.
+    /// </summary>
     public class SetCardsDownloadingEventArgs: EventArgs
     {
         /// <summary>
-        /// The declared number of cards, not considering translations).
-        /// It may be slightly off, for reasons known only to the Gatherer
-        /// programmers.
+        /// The declared number of cards, not considering translations.
         /// </summary>
         public readonly int NumCards;
 
@@ -19,6 +20,11 @@ namespace MagicRipper
         /// </summary>
         public readonly Set Set;
 
+        /// <summary>
+        /// Create a new SetCardsDownloadingEventArgs.
+        /// </summary>
+        /// <param name="set">The expansion this event is referring to.</param>
+        /// <param name="numCards">The declared number of cards, not considering translations.</param>
         public SetCardsDownloadingEventArgs(Set set,
             int numCards)
         {
